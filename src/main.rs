af3ff62c7f459e3c6435945fde9d13b86b232200
Ricky12Awesome <rickygraveen@gmail.com>
2021-10-18 13:32:25 -0500

    mem_percent

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![feature(format_args_capture)]
 
+use std::str::FromStr;
+
 use crate::bytes::{ByteFormat, ByteFormatConvert};
 use crate::fixed_system::FixedSystem;
 use crate::log::LogMode;
 use crate::placeholders::PlaceholderExpander;
+use crate::util::TrimTrailingZerosToString;
 
 mod bytes;
 mod fixed_system;
@@ -14,7 +17,6 @@ mod util;
 macro_rules! placeholder {
   ($($vis:vis $name:ident ($slf:ident, $args:ident) $b:block)+) => {
     $(
-      #[allow(unused)]
       $vis fn $name(&$slf, $args: &[&str]) -> String $b
     )+
 
@@ -36,6 +38,21 @@ type GetPlaceholder = placeholders::GetPlaceholder<Data>;
 
 impl Data {
   placeholder! {
+    mem_usage(self, args) {
+      let mut percision = 2;
+      let percent = (self.sys.used_memory() as f64 / self.sys.total_memory() as f64) * 100f64;
+
+      for arg in args {
+        if arg.starts_with('.') {
+          match usize::from_str(&arg[1..]) {
+            Ok(value) => percision = value,
+            Err(err) => log::warn!("{:?}", err)
+          }
+        }
+      }
+
+      percent.trim_trailing_zeros_with_precision(percision)
+    }
     mem_used(self, args) {
       self.mem_placeholder(FixedSystem::used_memory, args)
     }
@@ -60,12 +77,12 @@ impl Data {
   }
 
   fn mem_placeholder(&self, val: fn(&FixedSystem) -> u64, args: &[&str]) -> String {
-    let mut no_suffix = false;
+    let mut with_suffix = false;
     let mut format = ByteFormat::GiB;
 
     for arg in args {
       match *arg {
-        "no_suffix" => no_suffix = true,
+        "with_suffix" => with_suffix = true,
         "KB" | "kb" => format = ByteFormat::KB,
         "KiB" | "kib" => format = ByteFormat::KiB,
         "MB" | "mb" => format = ByteFormat::MB,
@@ -80,10 +97,10 @@ impl Data {
 
     let val = val(&self.sys).convert_to_display(ByteFormat::KiB, format);
 
-    if no_suffix {
-      val.to_string_no_suffix()
-    } else {
+    if with_suffix {
       val.to_string()
+    } else {
+      val.to_string_no_suffix()
     }
   }
 }
@@ -100,7 +117,7 @@ fn main() {
 
   let sys = FixedSystem::new_all();
   let data = Data { sys };
-  let pre_str = "${mem_used} ${mem_available|mib} ${mem_free} ${mem_total|KiB} ${this_should_not_exist}";
+  let pre_str = "${mem_usage|.2}% ${mem_usage|.6}%";
   let str = data.expand_placeholders(pre_str);
 
   log::debug!("Input \"{}\"", pre_str);
